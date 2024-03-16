73783e10cb09edeeb3cdc8044c92bf93f17a345c
jeffrey-steve <94464140+jeffrey-steve@users.noreply.github.com>
2024-03-16 07:17:19 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php 
+
+session_start();
+    include_once ("admin/class/adminback.php");
+    $obj = new adminback();
+
+    $cata_info = $obj-> p_display_catagory();
+    $cataDatas = array();
+    while($data = mysqli_fetch_assoc($cata_info)){
+        $cataDatas[]=$data;
+    }
+
+  
+
+
+
+
+
+
+
+?>
+
+
+
+
+<?php
+include_once("includes/head.php");
+?>
+
+<body class="biolife-body">
+    <!-- Preloader -->
+
+    <?php
+    include_once("includes/preloader.php");
+    ?>
+
+    <!-- HEADER -->
+    <header id="header" class="header-area style-01 layout-03">
+
+        <?php
+        include_once("includes/header_top.php");
+        ?>
+
+        <?php
+        include_once("includes/header_middle.php");
+        ?>
+
+        <?php
+        include_once("includes/header_bottom.php");
+        ?>
+
+    </header>
+
+    
+
+    <!-- FOOTER -->
+
+    <?php
+    include_once("includes/footer.php");
+    ?>
+
+    <!--Footer For Mobile-->
+    <?php
+    include_once("includes/mobile_footer.php");
+    ?>
+
+    <?php
+    include_once("includes/mobile_global.php")
+    ?>
+
+
+    <!-- Scroll Top Button -->
+    <a class="btn-scroll-top"><i class="biolife-icon icon-left-arrow"></i></a>
+
+    <?php
+    include_once("includes/script.php")
+    ?>
+</body>
+
+</html>
